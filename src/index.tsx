6632201a92a416a4385db6a6928d4467719a75f6
limunosekai <powerlsh0103@gmail.com>
2021-09-18 18:28:04 +0900

    Resetting the IFrame contents

--- src/index.tsx
+++ src/index.tsx
@@ -27,6 +27,8 @@ const App = () => {
       return;
     }
 
+    iframe.current.srcdoc = html;
+
     const result = await ref.current.build({
       entryPoints: ['index.js'],
       bundle: true,
@@ -57,6 +59,7 @@ const App = () => {
             } catch (err) {
               const root = document.querySelector('#root');
               root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              throw err;
             }
           }, false);
         </script>
