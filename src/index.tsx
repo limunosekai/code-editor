940a5572060c9a3cec4010fe63139eb76d0605b8
limunosekai <powerlsh0103@gmail.com>
2021-09-18 20:35:10 +0900

    Refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -1,86 +1,29 @@
-import * as esbuild from 'esbuild-wasm';
-import { useEffect, useState, useRef } from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import { unpkgPathPlugin } from './plugin/unpkg-path-plugin';
-import { fetchPlugin } from './plugin/fetch-plugin';
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
+import bundler from './bundler';
 
 const App = () => {
-  const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState('');
   const [input, setInput] = useState('');
 
-  // esbuild-wasm init
-  const startService = async () => {
-    ref.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-    });
-  };
-
-  useEffect(() => {
-    startService();
-  }, [])
-
   const onClick = async () => {
-    if (!ref.current) {
-      return;
-    }
-
-    iframe.current.srcdoc = html;
-
-    const result = await ref.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [
-        unpkgPathPlugin(),
-        fetchPlugin(input)
-      ],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
-    });
-
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
+    const output = await bundler(input);
+    setCode(output);
   };
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root"></div>
-        <script>
-          window.addEventListener('message', (event) => {
-            try {
-              eval(event.data);
-            } catch (err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              throw err;
-            }
-          }, false);
-        </script>
-      </body>
-    </html>
-  `;
 
   return (
     <div>
       <CodeEditor 
-        initialValue=""
+        initialValue="const a = 1;"
         onChange={(value) => setInput(value)} 
-      />
-      <textarea 
-        value={input} 
-        onChange={e => setInput(e.target.value)} 
       />
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe title="code preview" ref={iframe} srcDoc={html} sandbox="allow-scripts" />
+      <Preview code={code} />
     </div>
   );
 };
