6ba49340a6444a66fded8390d58d6e2571aa9060
limunosekai <powerlsh0103@gmail.com>
2021-09-19 20:34:12 +0900

    Add dark-theme & state

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,10 +1,11 @@
-import MDEditor from '@uiw/react-md-editor';
 import { useState, useEffect, useRef } from 'react';
-
+import MDEditor from '@uiw/react-md-editor';
+import './text-editor.css';
 
 const TextEditor: React.FC = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# Header');
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -22,15 +23,17 @@ const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div ref={ref}>
-        <MDEditor />
+      <div className="text-editor" ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
       </div>
     );
   }
 
   return (
-    <div onClick={() => setEditing(true)}>
-      <MDEditor.Markdown source={'# Header'} />
+    <div className="text-editor card" onClick={() => setEditing(true)}>
+      <div className="card-content">
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   )
 };
