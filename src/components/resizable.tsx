0f7f2540285d2876769dcc139dfe2ccd14ebfdd7
limunosekai <powerlsh0103@gmail.com>
2021-09-19 18:22:56 +0900

    Add Horizontal Resizing Func

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,4 @@
-import { ResizableBox } from 'react-resizable';
+import { ResizableBox, ResizableBoxProps } from 'react-resizable';
 import './resizable.css';
 
 interface ResizableProps {
@@ -6,14 +6,29 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  let resizableProps: ResizableBoxProps;
+  
+  if (direction === 'horizontal') {
+    resizableProps = {
+      className: 'resize-horizontal',
+      height: Infinity, 
+      width: window.innerWidth * 0.75,
+      resizeHandles: ['e'],
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [window.innerWidth * 0.2, Infinity]
+    };
+  } else {
+    resizableProps = {
+      height: 300, 
+      width: Infinity,
+      resizeHandles: ['s'],
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      minConstraints: [Infinity, 24]
+    };
+  }
+
   return (
-    <ResizableBox 
-      height={300} 
-      width={Infinity}
-      resizeHandles={['s']}
-      maxConstraints={[Infinity, window.innerHeight * 0.9]}
-      minConstraints={[Infinity, 24]}
-    >
+    <ResizableBox {...resizableProps}>
       {children}
     </ResizableBox>
   )
