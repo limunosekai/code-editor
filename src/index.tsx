2222aaaeb6d0af7a9fa76944cc466a46e8828002
limunosekai <powerlsh0103@gmail.com>
2021-09-21 00:44:47 +0900

    Add CellList, CellListItem Component && Custom Hooks

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
 import ReactDOM from 'react-dom';
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-// import CodeCell from './components/code-cell';
 import { Provider } from 'react-redux';
 import { store } from './state';
-import TextEditor from './components/text-editor';
+import CellList from './components/cell-list';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <div>
-        <TextEditor />
-      </div>
+      <>
+        <CellList />
+      </>
     </Provider>
   );
 };
